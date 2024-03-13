22807ff73255c47b8c229706854bf3482565ba25
MaksymFedorov22 <maxf7897@gmail.com>
2024-03-13 22:17:03 +0200

    com2

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#python1
+import os
+from pathlib import Path
+from dotenv import find_dotenv, load_dotenv
+
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path, verbose=True)
+print(os.getenv("TG_BOT_TOKEN1"))
