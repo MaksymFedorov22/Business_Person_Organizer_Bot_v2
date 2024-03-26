f822a7e957fc129de9831b3444791266a1826f22
MaksymFedorov22 <maxf7897@gmail.com>
2024-03-26 20:47:43 +0200

    new

--- event.py
+++
@@ -1,6 +0,0 @@
-class Event:
-    def __init__(self, name, date, description):
-        self.name = name
-        self.date = date
-        self.description = description
-        
--- main.py
+++ main.py
@@ -1,29 +1,55 @@
-import telebot
 from dotenv import load_dotenv
 import os
+import requests
+import json
 
 load_dotenv()
 
-tgBotToken = os.getenv('TG_BOT_TOKEN1')
+tgBotTOken = os.getenv('TG_BOT_TOKEN1')
+baseURL = f'https://api.telegram.org/bot{tgBotTOken}/'
 
-TgBot = telebot.TeleBot(tgBotToken)
-
-def SaveChatID(chat_id):
-    with open("chat_id.txt", "w") as file:
+def SaveMessage(chat_id):
+    with open("lasmsg.txt", "w") as file:
         file.write(str(chat_id))
-        
-def LoadChatID():
-    try:
-        with open("chat_id.txt", "r") as file:
-            return int(file.read().strip())
-    except FileNotFoundError:
-        return None
 
-@TgBot.message_handler(commands=['start'])
-def HandleStart(message):
-    ChatID = message.chat.id
-    SaveChatID(ChatID)
-    TgBot.send_message(ChatID, "Привіт! Я твій особистий органайзер.")
+def GetUpdates(offset=None):
+    url = baseURL + 'getUpdates'
+    params = {'timeout': 10}
+    if offset:
+        params['offset'] = offset
+    response = requests.get(url, params=params)
+    SaveMessage(response.content)
+    return response.json()
 
-TgBot.polling()
+def send_message(chat_id, text):
+    url = baseURL + 'sendMessage'
+    payload = {
+        'chat_id': chat_id,
+        'text': text
+    }
+    requests.post(url, json=payload)
 
+def main():
+    last_update_id = None
+
+    while True:
+        updates = get_updates(last_update_id)
+
+        if updates['result']:
+            for item in updates['result']:
+                update_id = item['update_id']
+                try:
+                    message = item['message']['text']
+                except:
+                    message = None
+
+                if message:
+                    chat_id = item['message']['chat']['id']
+                    send_message(chat_id, f'Вы написали: {message}')
+
+                if last_update_id is None or update_id >= last_update_id:
+                    last_update_id = update_id + 1
+
+if __name__ == '__main__':
+    main()
+    
