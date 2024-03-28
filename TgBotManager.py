f64125a10959f738e2e3d774a6a4208137454baf
MaksymFedorov22 <maxf7897@gmail.com>
2024-03-28 21:56:06 +0200

    28032024

--- TgBotManager.py
+++ TgBotManager.py
@@ -1,25 +1,29 @@
 from dotenv import set_key, get_key
 import requests
 import datetime
-#import json
 
 class TgBotManager:
     tgBotTOken = get_key('.env', 'TG_BOT_TOKEN1')
     baseURL = f'https://api.telegram.org/bot{tgBotTOken}/'
+    fnLoop = None
 
     def __init__(self):
+        return
+    
+    def SetLoop(self, fnLoop):
+        self.fnLoop = fnLoop
         return
 
     def SaveMessage(self, data):
         now = datetime.datetime.now()
         timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
-        
-        with open("lastMessage.txt", "a", encoding="utf-8") as file:
+        with open("bot.log", "a", encoding="utf-8") as file:
             file.write(f"{timestamp}|{str(data)}\n")
             
-    def GetUpdates(self, offset=None):
+    def GetUpdates(self):
         url = self.baseURL + 'getUpdates'
         params = {'timeout': 10}
+        offset = get_key('.env', 'OFFSET')
         if offset:
             params['offset'] = offset
         response = requests.get(url, params=params)
@@ -37,24 +41,17 @@ class TgBotManager:
 
     def Start(self):
         last_update_id = None
-
         while True:
-            updates = self.GetUpdates(last_update_id)
+            updates = self.GetUpdates()
+            if self.fnLoop != None:
+                ret = self.fnLoop(updates)
+                if ret == -1:
+                    break
 
             if updates['result']:
                 for item in updates['result']:
                     update_id = item['update_id']
-                    try:
-                        message = item['message']['text']
-                    except:
-                        message = None
+            if last_update_id is None or update_id >= last_update_id:
+                last_update_id = update_id + 1
+                set_key('.env', 'OFFSET', str(last_update_id))
 
-                    if message:
-                        chat_id = item['message']['chat']['id']
-                        self.SendMessage(chat_id, f'Вы написали: {message}')
-
-                    if last_update_id is None or update_id >= last_update_id:
-                        last_update_id = update_id + 1
-                        
-                        
-                        
--- main.py
+++ main.py
@@ -2,7 +2,23 @@ import TgBotManager
 
 bot1 = TgBotManager.TgBotManager()
 
+
+def MessageHandler(updates):
+    if updates['result']:
+        for item in updates['result']:
+            try:
+                message = item['message']['text']
+            except:
+                message = None
+
+            if message:
+                chat_id = item['message']['chat']['id']
+                bot1.SendMessage(chat_id, f'Вы написали: {message}')
+
+    return 0
+
 def main():
+    bot1.SetLoop(MessageHandler)
     bot1.Start()
 
 if __name__ == '__main__':
