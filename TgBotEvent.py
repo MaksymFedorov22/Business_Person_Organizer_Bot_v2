91d6d48b357051955552c8da04899ea6b889c812
MaksymFedorov22 <maxf7897@gmail.com>
2024-04-05 13:41:58 +0300

    2024-04-05MorningTgBotEvent

--- TgBotEvent.py
+++ TgBotEvent.py
@@ -1,4 +1,71 @@
-import TgBotManager
+import sqlite3
+from datetime import datetime, timedelta
 
 class TgBotEvent:
-    self.a = 0
+    def __init__(self, botManager):
+        self.botManager = botManager
+        self.conn = sqlite3.connect("test1.db")
+
+    def InitDbTable(self, conn):
+        curr = conn.cursor()
+        create_table_query = '''CREATE TABLE IF NOT EXISTS events (
+                                chatID INTEGER,
+                                eventType INTEGER,
+                                eventName TEXT NOT NULL,
+                                eventTime TIMESTAMP NOT NULL
+                            );'''
+        curr.execute(create_table_query)
+        conn.commit()
+        return 0
+
+    def SetEvent(self, chatID, eventType, eventName, eventTime):
+        curr = self.conn.cursor()
+        insert_query = '''INSERT INTO events (chatID, eventType, eventName, eventTime)
+                            VALUES (?, ?, ?, ?)'''
+        curr.execute(insert_query, (chatID, eventType, eventName, eventTime))
+        self.conn.commit()
+        return 0
+
+    def SetTimer3(self, msg):
+        chat_id = msg['chat']['id']
+        current_time = datetime.now()
+        alarm_time = current_time + timedelta(minutes=3)
+        self.SetEvent(chat_id, 0, "timer3", alarm_time)
+        self.botManager.SendMessage(chat_id, "Timer set")
+        return 0
+        
+    def SetPlannedEvent(self, msg):
+        chat_id = msg['chat']['id']
+        text = msg['text']
+        command, *args = text.split()
+        if len(args) < 2:
+            self.botManager.SendMessage(chat_id, "Please provide the event name and date in the format: /planedevent <event_name>, <event_date>")
+            return 0
+        
+        eventName = args[0]
+        strEventDate = ' '.join(args[1:])
+        try:
+            eventDate = datetime.strptime(strEventDate, "%Y-%m-%d %H:%M")
+            self.SetEvent(chat_id, 1, eventName, eventDate)
+            self.botManager.SendMessage(chat_id, f"Planned event '{eventName}' set for {strEventDate}")
+        except ValueError:
+            self.botManager.SendMessage(chat_id, "Invalid date format. Please use the format: YYYY-MM-DD HH:MM")
+            return 0
+
+        return 0
+
+    def CheckEvent(self):
+        cursor = self.conn.cursor()
+        current_time = datetime.now()
+        end_of_minute = current_time.replace(second=59, microsecond=999999)
+        select_query = "SELECT * FROM events WHERE eventTime < ?;"
+        cursor.execute(select_query, (end_of_minute, ))
+        events = cursor.fetchall()
+        for event in events:
+            msg = f"Reminder for your event {event[2]} at {event[3]}"
+            self.botManager.SendMessage(event[0], msg)
+            query2 = "DELETE FROM events WHERE chatID = ? AND eventTime = ?;"
+            cursor.execute(query2, (event[0], event[3]))
+            self.conn.commit()
+        return 0
+
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import TgBotManager
+import TgBotEvent
 import sqlite3
-from datetime import datetime, timedelta
 
 conn = sqlite3.connect("test1.db")
 
 bot1 = TgBotManager.TgBotManager()
+eventManager = TgBotEvent.TgBotEvent(bot1)
 
 commands = {}
 
@@ -14,80 +15,44 @@ def MessageStart(msg):
 Type /timer3 to set timer for 3 minutes.''')
     return 0
 
-def setTimer3(msg):
-    chat_id = msg['chat']['id']
-    currTime = datetime.now()
-    alarmTime = currTime + timedelta(minutes=3)
-    SetEvent(chat_id, 0, "timer3", alarmTime)
-    bot1.SendMessage(chat_id, "Timer set")
-
 def AddCommand(command, fnName):
     commands[command] = fnName
     return 0
 
-
 def InitDb(conn):
-    curr = conn.cursor()
-
-    create_table_query = '''CREATE TABLE IF NOT EXISTS events (
-                            chatID INTEGER,
-                            eventType INTEGER,
-                            eventName TEXT NOT NULL,
-                            eventTime TIMESTAMP NOT NULL
-                        );'''
-
-    curr.execute(create_table_query)
-
-    conn.commit()
+    eventManager.InitDbTable(conn)
     return 0
-
-def SetEvent(chatID, eventType, eventName, eventTime):
-    curr = conn.cursor()
-    insert_query = '''INSERT INTO events (chatID, eventType, eventName, eventTime)
-                        VALUES (?, ?, ?, ?)'''
-    curr.execute(insert_query, (chatID, eventType, eventName, eventTime))
-    conn.commit()
-    return 0
-    
-def checkTimer():
-    cursor = conn.cursor()
-    current_time = datetime.now()
-    start_of_minute = current_time.replace(second=0, microsecond=0)
-    end_of_minute = current_time.replace(second=59, microsecond=999999)
-    #end_of_minute = start_of_minute + timedelta(minutes=1)
-    # select_query = "SELECT * FROM events WHERE eventTime >= ? AND eventTime < ?;"
-    select_query = "SELECT * FROM events WHERE eventTime < ?;"
-    cursor.execute(select_query, (end_of_minute, ))
-    events = cursor.fetchall()
-    for event in events:
-        msg = f"Your timer {event[2]} at {event[3]}"
-        bot1.SendMessage(event[0], msg)
-        query2 = "DELETE FROM events WHERE chatID = ? AND eventTime = ?;"
-        cursor.execute(query2, (event[0], event[3]))
-        conn.commit()
-    return
 
 def MessageHandler(updates):
-    checkTimer()
+    eventManager.CheckEvent()
     if updates['result']:
         for item in updates['result']:
-            message = item['message']['text']
+            #message = item['message']['text']
+            if 'message' in item:
+                message = item['message']
+                txtMessage = item['message']['text']
+            elif 'edited_message' in item:
+                message = item['edited_message']
+                txtMessage = item['edited_message']['text']
+            else:
+                continue
             fProcessed = False
             for command, fnName in commands.items():
-                if message.startswith(command):
-                    fnName(item['message'])
+                if txtMessage.startswith(command):
+                    fnName(message)
                     fProcessed = True
                     break
             if (not fProcessed):
                 chat_id = item['message']['chat']['id']
-                bot1.SendMessage(chat_id, f'incorrect command {message}')
+                bot1.SendMessage(chat_id, f'incorrect command {txtMessage}')
     return 0
 
 def main():
     InitDb(conn)
     bot1.SetLoop(MessageHandler)
     AddCommand("/start", MessageStart)
-    AddCommand("/timer3", setTimer3)
+    AddCommand("/timer3", eventManager.SetTimer3)
+    AddCommand("/planedevent", eventManager.SetPlannedEvent)
     #
     bot1.Start()
 
